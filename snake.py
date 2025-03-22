1dd6f60bf3cf2507ff242c8403763ef68c9b4574
𝖟𝖊𝖕𝖍𝖞𝖗𝖚𝖘 <103388369+DaniloScimone@users.noreply.github.com>
2025-03-22 16:55:46 +0100

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,13 +1,13 @@
 # Snake by Danilo
-# movimento con le frecce
+# Usa le freccie direzionali per il movimento
 
 import turtle
 import random
 
-w = 500
-h = 500
-fs = 10
-d = 80
+# Configurazione dello schermo
+WIDTH, HEIGHT = 500, 500
+FOOD_SIZE = 10
+DELAY = 100  # Millisecondi tra ogni aggiornamento
 
 offsets = {
     "up": (0, 20),
@@ -16,92 +16,90 @@ offsets = {
     "right": (20, 0)
 }
 
-def r():
-    global saap, kata, khanaT, pen
-    saap = [[0, 0], [0, 20], [0, 40], [0, 60], [0, 80]]
-    kata = "up"
-    khanaT = nun()
-    food.goto(khanaT)
-    hall()
+def reset_game():
+    global snake, direction, food_position
+    snake = [[0, 0], [0, 20], [0, 40], [0, 60], [0, 80]]
+    direction = "up"
+    food_position = get_random_food_position()
+    food.goto(food_position)
+    update_game()
 
-def hall():
-    global kata
+def update_game():
+    global direction
+    
+    new_head = snake[-1].copy()
+    new_head[0] += offsets[direction][0]
+    new_head[1] += offsets[direction][1]
+    
+    if new_head in snake[:-1]:  # Collisione con se stesso
+        reset_game()
+        return
+    
+    snake.append(new_head)
+    
+    if not check_food_collision():
+        snake.pop(0)
+    
+    # Gestione attraversamento bordi
+    if new_head[0] > WIDTH / 2:
+        new_head[0] -= WIDTH
+    elif new_head[0] < -WIDTH / 2:
+        new_head[0] += WIDTH
+    elif new_head[1] > HEIGHT / 2:
+        new_head[1] -= HEIGHT
+    elif new_head[1] < -HEIGHT / 2:
+        new_head[1] += HEIGHT
+    
+    pen.clearstamps()
+    
+    for segment in snake:
+        pen.goto(segment[0], segment[1])
+        pen.stamp()
+    
+    screen.update()
+    turtle.ontimer(update_game, DELAY)
 
-    new_head = saap[-1].copy()
-    new_head[0] = saap[-1][0] + offsets[kata][0]
-    new_head[1] = saap[-1][1] + offsets[kata][1]
-
-    if new_head in saap[:-1]:  
-        r()
-    else:
-        saap.append(new_head)
-
-        if not khana():
-            saap.pop(0)  
-
-        if saap[-1][0] > w / 2:
-            saap[-1][0] -= w
-        elif saap[-1][0] < - w / 2:
-            saap[-1][0] += w
-        elif saap[-1][1] > h / 2:
-            saap[-1][1] -= h
-        elif saap[-1][1] < -h / 2:
-            saap[-1][1] += h
-
-        pen.clearstamps()
-
-        for segment in saap:
-            pen.goto(segment[0], segment[1])
-            pen.stamp()
-
-        screen.update()
-
-        turtle.ontimer(hall, d)
-
-def khana():
-    global khanaT
-    if dist(saap[-1], khanaT) < 20:
-        khanaT = nun()
-        food.goto(khanaT)
+def check_food_collision():
+    global food_position
+    if get_distance(snake[-1], food_position) < 20:
+        food_position = get_random_food_position()
+        food.goto(food_position)
         return True
     return False
 
-def nun():
-    x = random.randint(- w / 2 + fs, w / 2 - fs)
-    y = random.randint(- h / 2 + fs, h / 2 - fs)
-    return (x, y,)
+def get_random_food_position():
+    x = random.randint(-WIDTH // 2 + FOOD_SIZE, WIDTH // 2 - FOOD_SIZE)
+    y = random.randint(-HEIGHT // 2 + FOOD_SIZE, HEIGHT // 2 - FOOD_SIZE)
+    return x, y
 
-def dist(poos1, poos2):
-    x1, y1 = poos1
-    x2, y2 = poos2
-    distance = ((y2 - y1) ** 2 + (x2 - x1) ** 2) ** 0.5
-    return distance
+def get_distance(pos1, pos2):
+    return ((pos2[0] - pos1[0]) ** 2 + (pos2[1] - pos1[1]) ** 2) ** 0.5
 
-def mathi():
-    global kata
-    if kata != "down":
-        kata = "up"
+def move_up():
+    global direction
+    if direction != "down":
+        direction = "up"
 
-def go_right():
-    global kata
-    if kata != "left":
-        kata = "right"
+def move_right():
+    global direction
+    if direction != "left":
+        direction = "right"
 
-def go_down():
-    global kata
-    if kata != "up":
-        kata = "down"
+def move_down():
+    global direction
+    if direction != "up":
+        direction = "down"
 
-def go_left():
-    global kata
-    if kata != "right": 
-        kata = "left"
+def move_left():
+    global direction
+    if direction != "right":
+        direction = "left"
 
+# Impostazioni della finestra di gioco
 screen = turtle.Screen()
-screen.setup(w, h)
-screen.title("saap")
+screen.setup(WIDTH, HEIGHT)
+screen.title("Snake Game")
 screen.bgcolor("pink")
-screen.setup(500, 500)
 screen.tracer(0)
 
 pen = turtle.Turtle("square")
@@ -111,14 +109,15 @@ pen.penup()
 food = turtle.Turtle()
 food.shape("circle")
 food.color("red")
-food.shapesize(fs / 20) 
+food.shapesize(FOOD_SIZE / 20)
 food.penup()
 
+# Ascolto dei comandi da tastiera
 screen.listen()
-screen.onkey(mathi, "Up")
-screen.onkey(go_right, "Right")
-screen.onkey(go_down, "Down")
-screen.onkey(go_left, "Left")
+screen.onkey(move_up, "Up")
+screen.onkey(move_right, "Right")
+screen.onkey(move_down, "Down")
+screen.onkey(move_left, "Left")
 
-r ()
+reset_game()
 turtle.done()
